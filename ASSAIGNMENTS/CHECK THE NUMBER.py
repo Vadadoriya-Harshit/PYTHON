b0d53184b4e92bcb02f3e0d59332d3daab9a040a
Harshuv9 <harshitpatelv9@gmail.com>
2023-03-23 09:40:37 +0530

    Create CHECK THE NUMBER.py

---
+++ ASSAIGNMENTS/CHECK THE NUMBER.py
@@ -0,0 +1,10 @@
+number=int(input("\n\tEnter Any Number :"))
+print("\n\t Number is : ",number)
+
+if number>0:
+    print("\n\tThe Number is positive")
+elif number==0:
+    print("\n\tThe Number is Zero")
+else:
+    print("\n\tThe Number is Nagative")
+
